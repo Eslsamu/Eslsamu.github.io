537fe88979fd632a0e3599814d4d98322ea4a612
Eslsamu <samuel.r.kopp@gmail.com>
2020-10-07 22:22:16 +0200

    finished

--- js/index.js
+++ js/index.js
@@ -113,7 +113,7 @@ particlesJS("particles-js", {
 /* ---- Typed.js config ---- */
 var typed = new Typed('#typed', {
   stringsElement: '#typed-strings',
-  typeSpeed: 40,
+  typeSpeed: 50,
   backSpeed: 25,
   backDelay: 900,
   loop: true,
